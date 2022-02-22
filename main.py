55755a8e006d2c291eabb00f55e520e9de0e29e9
sincuks <91064799+sintijinja@users.noreply.github.com>
2022-02-22 13:03:47 +0000

    viss

--- main.py
+++ main.py
@@ -6,4 +6,16 @@ app = Flask('app')
 def index():
   return render_template("index.html")
 
+@app.route('/page.html')
+def page():
+  return render_template("page.html")
+
+@app.route('/receptes.html')
+def receptes():
+  return render_template("receptes.html")
+
+@app.route('/rcp.html')
+def rcp():
+  return render_template("rcp.html")
+
 app.run(host="0.0.0.0",port= 8080)
